f466f32f8ac67e2363a79b430d1177d3168385fd
Jassar Al-Akhras <JAlakhras@blessedtreeit.com>
2020-03-29 00:05:27 +0300

    Add project files.

---
+++ Signature/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Signature.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
